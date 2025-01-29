867f8200becc3c9d4b7fc8a55a208a91ea5767c6
TerryxEmilia <terryxemilia@gmail.com>
2025-01-29 20:51:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function clickFunction(id){
+    document.getElementById(id).style.color = "black";
+}
+function changeColor(id){
+    document.getElementById(id).style.color = "black";
+}
+function darkMode(id){
+    document.getElementById('home').style.backgroundColor = "black";
+    document.getElementById('about').style.backgroundColor = "black";
+    document.getElementById('context').style.backgroundColor = "black";
+    document.getElementById('test').style.backgroundColor = "black";
+}
+function whiteMode(id){
+    document.getElementById('home').style.backgroundColor = "white";
+    document.getElementById('about').style.backgroundColor = "white";
+    document.getElementById('context').style.backgroundColor = "white";
+    document.getElementById('test').style.backgroundColor = "white";
+    document.getElementsByTagName('p') = "blue";
+}
